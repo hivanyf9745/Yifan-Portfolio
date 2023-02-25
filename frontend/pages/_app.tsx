6a009c1e4afb72fa9beb1cdc1061fe252f1dc9a6
Ivan Huang <yifanhuang.Ivan@hotmail.com>
2023-02-24 22:50:57 -0500

    Added part of the fonts to the project, and got familiar with the font-adding process

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+// importing font properties
+import localFont from "@next/font/local";
+
+// Local Fonts Declarations
+const okuda = localFont({
+  src: [
+    { path: "../public/fonts/Okuda.otf", weight: "500" },
+    { path: "../public/fonts/Okuda-Bold.otf", weight: "700" },
+  ],
+});
+
+const oxanium = localFont({
+  src: [{ path: "../public/fonts/Oxanium-VariableFont_wght.ttf" }],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --okuda-font: ${okuda.style.fontFamily};
+            --oxanium-font: ${oxanium.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
