2360581bcc4b09a1fdbf3574154f6da592e5b693
Ivan Huang <yifanhuang.Ivan@hotmail.com>
2023-03-15 12:11:51 -0400

    Added all sub navigation sections

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        {/* If you want to add bootstrap javascript using bootstrap cdn */}
+        {/* <script
+          src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js'
+          integrity='sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3'
+          crossOrigin='anonymous'></script>
+        <script
+          src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js'
+          integrity='sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD'
+          crossOrigin='anonymous'></script> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
